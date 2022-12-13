c5e3de25eb9cad57d3187bb27546e0728707b515
Jaspreet Singh <jaspreetsingh2379@gmail.com>
2022-12-13 20:25:43 +0530

    rustfmt added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-  let name = "akin";
-  let mut age = 35;
-  age += 1;
-  if name.len() > 0 {
-      println!(
-          "Hello, world!, my name is {}, and I am {} years old",
-          name, age
-      );
-  }
+	let name = "akin";
+	let mut age = 35;
+	age += 1;
+	if name.len() > 0 {
+		println!(
+			"Hello, world!, my name is {}, and I am {} years old",
+			name, age
+		);
+	}
 }
