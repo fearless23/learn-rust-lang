76ed865846fdffa0bf01b40bda753840e8d77af5
Jaspreet Singh <jassi02412@gmail.com>
2022-12-21 06:41:31 +0000

    day1,day2 added

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,17 @@
 fn main() {
-	let numbers = get_numbers();
-	let sum = top_three_groups(numbers);
-	println!("Top three total is {:?}", sum);
+	let moves = get_moves();
+	println!("MOVES {:?}", moves);
+  let score = calculate_score(moves);
+	println!("SCORE {:?}", score);
 }
 
-fn get_numbers() -> std::vec::Vec<u64> {
-	let input = std::fs::read_to_string("src/read_file/input.txt").unwrap();
-	let input = input.lines().map(|i| i.parse::<u64>().unwrap_or(0));
-	input.collect::<Vec<u64>>()
+fn get_moves() -> Vec<Vec<String>> {
+	let input = std::fs::read_to_string("src/aoc2022/day2/demo.txt").unwrap();
+	let input = input
+		.lines()
+		.map(|i| i.split(' ').map(|j| j.to_string()).collect());
+	input.collect::<Vec<Vec<String>>>()
 }
 
-fn top_three_groups(numbers: Vec<u64>) -> u64 {
-	let mut groups: Vec<u64> = Vec::new();
-	let mut sum: u64 = 0;
-	for num in numbers {
-		if num == 0 {
-			groups.push(sum);
-			sum = 0;
-		} else {
-			sum += num;
-		}
-	}
-
-	let mut sm: u64 = groups[0];
-	let mut md: u64 = groups[1];
-	let mut lg: u64 = groups[2];
-
-	for num in groups {
-		if num < sm {
-			continue;
-		}
-		if num <= md {
-			sm = num;
-		} else {
-			sm = md;
-			if num <= lg {
-				md = num;
-			} else {
-				md = lg;
-				lg = num;
-			}
-		}
-	}
-	sm + md + lg
+fn calculate_score(moves:Vec<Vec<String>>) -> u32 {
 }
